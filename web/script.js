cac69b6d55b957b9aa86ff34017b325c2bc3cb4a
reyesisamar455-svg <reyesisamar455@gmail.com>
2026-04-05 20:40:52 -0600

    Update script.js

--- web/script.js
+++ web/script.js
@@ -1,1 +1,14 @@
+function publicar() {
+    const estado = document.getElementById("estado")
+    estado.innerText = "⏳ Publicando..."
 
+    fetch("https://TU-SERVIDOR.com/publicar", {
+        method: "POST",
+        headers: {
+            "clave": "1234"
+        }
+    })
+    .then(res => res.text())
+    .then(data => estado.innerText = data)
+    .catch(() => estado.innerText = "❌ Error")
+}
